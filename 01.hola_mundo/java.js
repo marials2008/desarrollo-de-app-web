bedda1d7044142b833cfe849b2a48afba2139338
marials2008 <mariasandoval@liceovvh.cl>
2025-03-25 10:27:00 -0300

    ok

---
+++ 01.hola_mundo/java.js
@@ -0,0 +1,5 @@
+console.log("Funcionando correctamente")
+
+function saludo(){
+    alert("Holiwis")
+};
---
+++ 02.tipo_de_variable/script.js
@@ -0,0 +1,20 @@
+console.log("Funcionando correctamente")
+
+function saludo(){
+    alert("Holiwis")
+};
+
+//Creacion de variables
+
+let numero = 0; //numerica
+let decimal = 2.5;//num float
+let texto = "Holap";//string
+let booleano = true;//bool
+
+let array = [1, "hola", 25];//lista de elementos
+let objeto = {nombre: "Max", edad: 18};
+
+
+function sumar(a, b){
+    return a + b
+};
