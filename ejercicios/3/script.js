0734bbe3b7ce12725b65be6890f0ee7f30010e07
marials2008 <mariasandoval@liceovvh.cl>
2025-03-31 14:57:24 -0300

    ok

---
+++ ejercicios/3/script.js
@@ -0,0 +1,10 @@
+function Suma_indices() {
+    let array = [1,2,3,4,5,6,7];
+    let suma = array[0] +array[1] +array[2] +array[3] +array[4] +array[5] +array[6];
+    alert('Los numeros son: ' + array + '\nEl resultado es: ' + suma);
+}
+
+function pares() {
+    let arrelo = [2,4,6,7,10,12];
+    let producto = ;
+}
