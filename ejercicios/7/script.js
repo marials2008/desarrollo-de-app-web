f62a0470366b709432f86e8a272abe4a8c0544df
marials2008 <165289029+marials2008@users.noreply.github.com>
2025-06-04 09:27:48 -0400

    ok

---
+++ ejercicios/7/script.js
@@ -0,0 +1,19 @@
+//Primero
+
+function tablaMultiplicar() {
+    let cifra = prompt('Ingrese su numero: ');
+    
+    let multiplicador = 1; 
+
+    while(multiplicador <= 10) {
+        let resultado = cifra * multiplicador;
+        alert(cifra + 'x' + multiplicador + '=' + resultado);
+        multiplicador++;
+    }
+}
+
+//Segundo
+
+function suma(){
+    
+}
