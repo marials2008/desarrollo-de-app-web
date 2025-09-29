8c094bac92f7eb665cb1936a337b15d017d2dc4a
marials2008 <mariasandoval@liceovvh.cl>
2025-09-29 10:42:31 -0300

    ok

---
+++ 12_this_js/js/script.js
@@ -0,0 +1,16 @@
+Javascript:
+console.log("page loaded...");
+
+function liked(element) {
+    const span = element.querySelector("span");
+    let like =
+}
+
+
+function login(element) {
+    if(element.innerText)
+    }
+
+function hide(element) {
+    
+}
