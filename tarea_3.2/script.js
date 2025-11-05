d5dcd9a9e3bbb262e7b0666536fc80f5ec9fa0cc
marials2008 <mariasandoval@liceovvh.cl>
2025-11-05 12:41:56 -0300

    ok

---
+++ tarea_3.2/script.js
@@ -0,0 +1,19 @@
+let cont =9
+
+function like(){
+    cont++
+    let likes = `${cont} Like(s)`
+    document.getElementById("like").innerText = likes
+}
+let cont2 =12
+function like2(){
+    cont2++
+    let likes = `${cont2} Like(s)`
+    document.getElementById("like2").innerText = likes
+}
+let cont3 =9
+function like3(){
+    cont3++
+    let likes = `${cont3} Like(s)`
+    document.getElementById("like3").innerText = likes
+}
